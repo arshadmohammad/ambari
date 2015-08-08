06cf4f90b43ed4127b977528738119d1dbc5d5ef
arshadmohammad <arshad.mohammad.k@gmail.com>
2015-08-08 20:57:26 +0530

    db check script

---
+++ ambari-server/conf/unix/dbCheck.sh
@@ -0,0 +1,1 @@
+java -cp ../resources/DBConnectionVerification.jar:../resources/mysql-connector-java-5.1.7-bin.jar org.apache.ambari.server.DBConnectionVerification $1 root root com.mysql.jdbc.Driver
